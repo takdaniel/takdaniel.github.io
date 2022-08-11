caceb6e052ad9e3e8af19bc792b08307c7b0ac83
takdaniel <89981385+takdaniel@users.noreply.github.com>
2022-08-11 19:01:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-let daniPoints = document.getElementById("danipoints")
+const matchEl = document.getElementById("match-el")
+const daniPlus = document.getElementById("increase-btn-dani")
+const daniMinus = document.getElementById("decrease-btn-dani")
+const zsaniPlus = document.getElementById("increase-btn-zsani")
+const zsaniMinus = document.getElementById("decrease-btn-zsani")
 
-let zsaniPoints = document.getElementById("zsanipoints")
-
-let matchEl = document.getElementById("match-el")
-
-
-
-let pointOfDani = 0
-
-let pointOfZsani = 0
+let daniPoints = 0
+let zsaniPoints = 0
 
 
-function increaseDaniPoints() {
-    pointOfDani = pointOfDani + 1
-    daniPoints.innerText = pointOfDani
+daniPlus.addEventListener('click', function(){
+    daniPoints = daniPoints + 1
+    localStorage.setItem("daniPontok", JSON.stringify(daniPoints) )
+    window.location.reload()
+})
+
+daniMinus.addEventListener('click', function(){
+    daniPoints = daniPoints - 1 
+    localStorage.setItem("daniPontok", JSON.stringify(daniPoints) )
+    window.location.reload()
+})
+
+daniPoints = JSON.parse(localStorage.getItem("daniPontok") )
+
+zsaniPlus.addEventListener('click', function(){
+    zsaniPoints = zsaniPoints + 1 
+    localStorage.setItem("zsaniPontok", JSON.stringify(zsaniPoints))
+    window.location.reload()
+})
+
+zsaniMinus.addEventListener('click', function(){
+    zsaniPoints = zsaniPoints - 1
+    localStorage.setItem("zsaniPontok", JSON.stringify(zsaniPoints) )
+    window.location.reload()
+})
+
+zsaniPoints = JSON.parse(localStorage.getItem("zsaniPontok") )
+
+let message = ""
+if(zsaniPoints > daniPoints){
+    message = "Zsani áll nyerésre!"
+} else if(zsaniPoints === daniPoints){
+    message = "Jelenleg döntetlen az állás!"
+} else{
+    message = "Dani áll nyerésre!"
 }
 
-function increaseZsaniPoints() {
-    pointOfZsani = pointOfZsani + 1
-    zsaniPoints.innerText = pointOfZsani
-}
-
-function decreaseDaniPoints() {
-    pointOfDani = pointOfDani - 1
-    daniPoints.innerText = pointOfDani
-}
-
-function decreaseZsaniPoints() {
-    pointOfZsani = pointOfZsani - 1
-    zsaniPoints.innerText = pointOfZsani
-}
-
-function showResult() {
-    matchEl.textContent += pointOfDani + " - " + pointOfZsani
-
-}
+matchEl.innerHTML = `Jelenlegi állás: ${daniPoints} - ${zsaniPoints} <br> ${message}`
