88811afacf325339e6d6a47846f8123ec69ee753
takdaniel <89981385+takdaniel@users.noreply.github.com>
2022-08-21 20:33:43 +0200

    Add files via upload
    reward system

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const daniPlus = document.getElementById("increase-btn-dani")
 const daniMinus = document.getElementById("decrease-btn-dani")
 const zsaniPlus = document.getElementById("increase-btn-zsani")
 const zsaniMinus = document.getElementById("decrease-btn-zsani")
+const winnerEl = document.getElementById("winner-el")
+const zsaniPrize = ["valamilyen kaja", "egy masszázs", "egy turizás", "egy üveg kóla", "egy doboz heets", "fejmasszázs", "2 doboz energiaital"]
+const daniPrize = ["szopás", "ciciszex", "popószex", "masszázs", "egy doboz cigi", "egy üveg kóla", "footjob", "üdítő vagy energiaital", ]
+const resetBtn = document.getElementById("reset-btn")
 
 let daniPoints = 0
 let zsaniPoints = 0
@@ -46,3 +50,35 @@ if(zsaniPoints > daniPoints){
 }
 
 matchEl.innerHTML = `Jelenlegi állás: ${daniPoints} - ${zsaniPoints} <br> ${message}`
+
+let announcement = ""
+if (zsaniPoints === 20) {
+    announcement = "Zsani nyert! Nyereménye "
+    randomIndex = Math.floor(Math.random() * zsaniPrize.length)
+    winnerEl.innerHTML = `${announcement} ${zsaniPrize[randomIndex]}`
+    daniMinus.disabled = true
+    daniPlus.disabled = true
+    zsaniMinus.disabled = true
+    zsaniPlus.disabled = true
+    matchEl.innerHTML = "Verseny vége."
+} else if (daniPoints === 20) {
+    announcement = "Dani nyert! Nyereménye "
+    randomIndex = Math.floor(Math.random() * daniPrize.length)
+    winnerEl.innerHTML = `${announcement} ${daniPrize[randomIndex]}`
+    daniMinus.disabled = true
+    daniPlus.disabled = true
+    zsaniMinus.disabled = true
+    zsaniPlus.disabled = true
+    matchEl.innerHTML = "Verseny vége."
+}
+
+resetBtn.addEventListener('click', function(){
+    localStorage.clear()
+    window.location.reload()
+    matchEl.innerHTML = `Jelenlegi állás: ${daniPoints} - ${zsaniPoints} <br> ${message}`
+    daniMinus.disabled = false
+    daniPlus.disabled = false
+    zsaniMinus.disabled = false
+    zsaniPlus.disabled = false
+})
+
